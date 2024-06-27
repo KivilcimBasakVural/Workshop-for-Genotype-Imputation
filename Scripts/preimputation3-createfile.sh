ad05a4ee75372185fdd322a5230bdfa1a1f1a0f7
KivilcimBasakVural <74351346+KivilcimBasakVural@users.noreply.github.com>
2024-06-27 16:22:43 +0300

    Update preimputation3-createfile.sh

--- Scripts/preimputation3-createfile.sh
+++ Scripts/preimputation3-createfile.sh
@@ -2,7 +2,7 @@
 
 ###creating the files 
 
-for bam in $(cat bamlist1);do sample="$( cut -d "_" -f1 <<< "${bam}" )"; samplename=${sample##*/}; echo "$samplename";done >list
+for bam in $(cat bamlist);do sample="$( cut -d "_" -f1 <<< "${bam}" )"; samplename=${sample##*/}; echo "$samplename";done >list
 for i in $(cat list);do mkdir ${i};done
 for i in $(cat list);do mkdir ${i}/${i}_bam;done
 for i in $(cat list);do mkdir ${i}/${i}_vcf;done
